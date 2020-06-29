4a6540b61fd2b06bd17c25fd0508987cb6666f96
Ciprian Manea <ciprian.manea@gmail.com>
2020-06-29 22:37:56 +0300

    ping dns

--- files/dynrun.sh
+++ files/dynrun.sh
@@ -2,3 +2,8 @@
 
 # ignora interfata lo(cala)
 [ "$IFACE" = "lo" ] &&  exit 0
+
+if [ -f /root/.unpi/profile.dynpin ]; then
+  pin=$(cat /root/.unpi/profile.dynpin)
+  curl -Ls "http://dyn.unpi.ro/$pin" -o /dev/null
+fi
