ee02c0fa7216c16b6ecede8e38ba770e8fcbe9c4
Ciprian Manea <ciprian.manea@gmail.com>
2020-06-30 17:48:56 +0300

    Update dynrun.sh

--- files/dynrun.sh
+++ files/dynrun.sh
@@ -6,5 +6,5 @@
 if [ -f /root/.unpi/profile.dynpin ]; then
   pin=$(cat /root/.unpi/profile.dynpin)
   wget -q --read-timeout=0.0 --waitretry=5 --tries=12 \
-    --background "http://dyn.unpi.ro/$pin" >/dev/null
+    --background "http://dyn.unpi.ro/$pin" -O /dev/null > /dev/null
 fi
