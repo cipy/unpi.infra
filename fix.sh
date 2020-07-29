4065e01b10a07d57789450edbd8c4f401a3a7847
Ciprian Manea <ciprian.manea@gmail.com>
2020-07-29 17:35:12 +0300

    Update fix.sh

--- fix.sh
+++ fix.sh
@@ -1,5 +1,5 @@
 #!/bin/dash
 
-sudo raspi-config nonint do_overlayfs 1
+logger unpi.fixing.bit
 
 curl -s http://ping.unpi.ro/ping/fix -A "$(cat /root/.unpi/profile.token | md5sum | cut -d' ' -f1)" -o /dev/null
