f4a2faa01b915ebf9f34f88ed871be9b7a5f3744
Ciprian Manea <ciprian.manea@gmail.com>
2020-07-29 18:07:18 +0300

    re

--- zero.sh
+++ zero.sh
@@ -2,7 +2,7 @@
 # zero quick
 
 echo
-echo "Acum facem o configurare specifica pentru unPi mini"
+echo "Acum facem o re.configurare specifica pentru unPi mini"
 echo
 wget -q https://infra.unpi.ro/zero.yml -O zero.yml
 
