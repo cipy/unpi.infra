8043681ba16e58ab56d236005a5ebe84c62d41e6
Ciprian Manea <ciprian.manea@gmail.com>
2020-10-14 01:55:24 +0300

    Create get.scratch.sh

---
+++ files/get.scratch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/Desktop/Scratch.Video
+cd ~/Desktop/Scratch.Video
+
+figlet aduc lectiile de Scratch 3
+for mov in aspect control detectare evenimente miscare operatori; do
+  echo acum aduc $mov.mp4
+  wget -nv -c --show-progress http://cache.unpi.ro/cache/scratch/old/$mov.mp4
+done
+
+cd
+figlet gata.
