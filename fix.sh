a151fcc6ae285219095d3a7cb705f4aa6871c563
Ciprian Manea <ciprian.manea@gmail.com>
2020-11-26 09:43:37 +0200

    disable ssh

--- fix.sh
+++ fix.sh
@@ -2,4 +2,7 @@
 
 logger unpi.fixing.bit
 
+# daca imaginea NOOBS l-a activat
+systemctl disable ssh
+
 curl -s http://ping.unpi.ro/ping/fix -A "$(cat /root/.unpi/profile.token | md5sum | cut -d' ' -f1)" -o /dev/null
