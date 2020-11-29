3f22c6906ef80a00d3fab5eb66bb45f4c5e635a7
Ciprian Manea <ciprian.manea@gmail.com>
2020-11-29 19:18:24 +0200

    Update get.scratch.sh

--- files/get.scratch.sh
+++ files/get.scratch.sh
@@ -3,11 +3,11 @@
 mkdir -p ~/Desktop/"Scratch 3 Curs Video"
 cd ~/Desktop/"Scratch 3 Curs Video"
 
-figlet aduc lectiile de Scratch 3
+[ -z "$NOFIG" ] && figlet aduc lectiile de Scratch 3
 for mov in aspect control detectare evenimente miscare operatori; do
   echo acum aduc $mov.mp4
   wget -nv -c --show-progress http://cache.unpi.ro/cache/scratch/old/$mov.mp4
 done
 
 cd
-figlet gata.
+[ -z "$NOFIG" ] && figlet gata.
