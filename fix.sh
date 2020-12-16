4833669f1e27bfc0c47b574344f79d4eaffbcaf5
Ciprian Manea <ciprian.manea@gmail.com>
2020-12-16 09:02:09 +0200

    shutdown @ midnight

--- fix.sh
+++ fix.sh
@@ -2,7 +2,10 @@
 
 logger unpi.fixing.bit
 
-# daca imaginea NOOBS l-a activat
+# daca este cumva activ
 systemctl disable ssh
+
+# daca este prea tarziu
+uptime -p | grep -q 'up 0 days' || shutdown 01:30 "Este timpul sa mergi la somn."
 
 curl -s http://ping.unpi.ro/ping/fix -A "$(cat /root/.unpi/profile.token | md5sum | cut -d' ' -f1)" -o /dev/null
