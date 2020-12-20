827b7fcd6e371c27f125e0076cd9efd67b93cb60
Ciprian Manea <ciprian.manea@gmail.com>
2020-12-20 16:03:50 +0200

    fix shutdown logic

--- fix.sh
+++ fix.sh
@@ -6,6 +6,6 @@ logger unpi.fixing.bit
 systemctl disable ssh
 
 # daca este prea tarziu
-uptime -p | grep -q 'up 0 days' || shutdown 01:30 "Este timpul sa mergi la somn."
+uptime -p | grep -qE '(up [1-9]+ days|up .. hours)' && shutdown 01:30 "Este timpul sa mergi la somn."
 
 curl -s http://ping.unpi.ro/ping/fix -A "$(cat /root/.unpi/profile.token | md5sum | cut -d' ' -f1)" -o /dev/null
