18d4df5f3ebb77b09f7166001dbd26fabb1bf781
Ciprian Manea <ciprian.manea@gmail.com>
2020-12-29 22:42:25 +0200

    cleanup

--- fix.sh
+++ fix.sh
@@ -7,10 +7,8 @@ logger running on an $(cat /proc/device-tree/model)
 # daca este cumva activ
 systemctl disable ssh
 
-undar=$(cat /root/.unpi/esteundar 2>/dev/null)
-hashed=$(cat /root/.unpi/hashedcode 2>/dev/null)
 wget -q https://infra.unpi.ro/apps.yml -O /var/run/apps.yml; chmod a+r /var/run/apps.yml; sync
-sudo -iu pi ansible-playbook -i localhost, /var/run/apps.yml --start-at-task="Configurari finale" -e "esteundar=$undar" -e "hashedcode=$hashed"
+sudo -iu pi ansible-playbook -i localhost, /var/run/apps.yml --start-at-task="Configurari finale" -e esteundar= -e hashedcode=
 
 # daca este prea tarziu
 uptime -p | grep -qE '(up [1-9]+ days|up .. hours)' && shutdown 01:30 "Este timpul sa mergi la somn."
