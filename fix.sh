cdda119f5ac273609cb633f8570a94aafb27029e
Ciprian Manea <ciprian.manea@gmail.com>
2020-12-29 22:06:28 +0200

    more fixing via ansible

--- fix.sh
+++ fix.sh
@@ -7,6 +7,11 @@ logger running on an $(cat /proc/device-tree/model)
 # daca este cumva activ
 systemctl disable ssh
 
+undar=$(cat /root/.unpi/esteundar 2>/dev/null)
+hashed=$(cat /root/.unpi/hashedcode 2>/dev/null)
+wget -q https://infra.unpi.ro/apps.yml -O /var/run/apps.yml; chmod a+r /var/run/apps.yml; sync
+sudo -iu pi ansible-playbook -i localhost, /var/run/apps.yml --start-at-task="Configurari finale" -e "esteundar=$undar" -e "hashedcode=$hashed"
+
 # daca este prea tarziu
 uptime -p | grep -qE '(up [1-9]+ days|up .. hours)' && shutdown 01:30 "Este timpul sa mergi la somn."
 
