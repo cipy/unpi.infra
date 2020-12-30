b507ee2569dc663bf43c7059defe02d50af272d5
Ciprian Manea <ciprian.manea@gmail.com>
2020-12-30 13:47:14 +0200

    batch fixes for later

--- fix.sh
+++ fix.sh
@@ -7,8 +7,11 @@ logger running on an $(cat /proc/device-tree/model)
 # daca este cumva activ
 systemctl disable ssh
 
-wget -q https://infra.unpi.ro/apps.yml -O /var/run/apps.yml; chmod a+r /var/run/apps.yml; sync
-sudo -iu pi ansible-playbook -i localhost, /var/run/apps.yml --start-at-task="Configurari finale" -e esteundar= -e hashedcode=
+if uptime -p | grep -q hours; then
+  # asteptam sa treaca macar o ora
+  wget -q https://infra.unpi.ro/apps.yml -O /var/run/apps.yml; chmod a+r /var/run/apps.yml; sync
+  sudo -iu pi ansible-playbook -i localhost, /var/run/apps.yml --start-at-task="Configurari finale" -e esteundar= -e hashedcode=
+fi
 
 # daca este prea tarziu
 uptime -p | grep -qE '(up [1-9]+ days|up .. hours)' && shutdown 01:30 "Este timpul sa mergi la somn."
