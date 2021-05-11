e6e9d708b6a95dfe64b5542bfd18cf796ef154c6
Ciprian Manea <ciprian.manea@gmail.com>
2021-05-11 07:17:35 +0300

    force to IPv4

--- zero.sh
+++ zero.sh
@@ -4,7 +4,7 @@
 echo
 echo "Acum facem o re.configurare specifica pentru unPi mini"
 echo
-wget -q https://infra.unpi.ro/zero.yml -O zero.yml
+wget -4 -q https://infra.unpi.ro/zero.yml -O zero.yml
 
 export ANSIBLE_STDOUT_CALLBACK=unixy
 [ -s zero.yml ] && ansible-playbook -i localhost, zero.yml
