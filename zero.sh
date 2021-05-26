2211a44b0563b6804068d43a87294141199d10df
Ciprian Manea <ciprian.manea@gmail.com>
2021-05-26 21:50:06 +0300

    Update zero.sh

--- zero.sh
+++ zero.sh
@@ -6,8 +6,11 @@ echo "Acum facem o re.configurare specifica pentru unPi mini"
 echo
 wget -4 -q https://infra.unpi.ro/zero.yml -O zero.yml
 
+touch /tmp/run.init.sh
 export ANSIBLE_STDOUT_CALLBACK=unixy
 [ -s zero.yml ] && ansible-playbook -i localhost, zero.yml
 
 echo
 figlet gata
+rm -f /tmp/run.init.sh
+sync
