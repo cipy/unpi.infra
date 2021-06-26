b9f9395077a39dbe530ca77c91727db41a7ac412
Ciprian Manea <ciprian.manea@gmail.com>
2021-06-26 17:48:35 +0300

    speedtest

--- files/gui/warn.py
+++ files/gui/warn.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 
+import os
 from guizero import App, Text, Picture
+
+def testSpeed():
+  with open("/tmp/speed", 'a+') as f:
+    f.seek(0); speed.value = f.read()
+
+run = os.system("speedtest --simple | tee /tmp/speed | logger &")
 
 app = App(title="Atentie")
 line = Text(app, "")
@@ -8,12 +15,16 @@ info = Text(app, text=">>> calculatorul unPi isi face acum update <<<")
 stop = Text(app, text="te rog nu-l opri inca!")
 line2 = Text(app, "")
 image = Picture(app, image="/usr/share/cups/doc-root/images/wait.gif")
+line3 = Text(app, "")
+speed = Text(app, "")
 
 line.text_size = 30
 info.text_size = 20
 stop.text_size = 35
 line2.text_size = 20
+line3.text_size = 20
 image.resize(100,100)
 
+app.repeat(30*1000, testSpeed)
 app.bg = "yellow"
 app.display()
