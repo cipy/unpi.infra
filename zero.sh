e92412bbbe48df6e857a3a6293e5126db2ddf0fe
Ciprian Manea <ciprian.manea@gmail.com>
2021-10-02 15:10:22 +0300

    IPv6 is ok now

--- zero.sh
+++ zero.sh
@@ -4,7 +4,7 @@
 echo
 echo "Acum facem o re.configurare specifica pentru unPi mini"
 echo
-wget -4 -q https://infra.unpi.ro/zero.yml -O zero.yml
+wget -q https://infra.unpi.ro/zero.yml -O zero.yml
 
 touch /tmp/run.init.sh
 export ANSIBLE_STDOUT_CALLBACK=unixy
