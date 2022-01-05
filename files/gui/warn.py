2f4e76cc1dbe11e293d6076f152cd3590146b8a2
Ciprian Manea <ciprian.manea@gmail.com>
2022-01-05 19:23:37 +0200

    emojis

--- files/gui/warn.py
+++ files/gui/warn.py
@@ -15,6 +15,10 @@ info = Text(app, text=">>> calculatorul unPi isi face acum update <<<")
 stop = Text(app, text="te rog nu-l opri inca!")
 line2 = Text(app, "")
 image = Picture(app, image="/usr/share/cups/doc-root/images/wait.gif")
+line30 = Text(app, "")
+try:
+  image2 = Picture(app, image="/var/tmp/emojis/1f0cf.gif")
+except: image2 = image
 line3 = Text(app, "")
 speed = Text(app, "")
 
@@ -22,8 +26,10 @@ line.text_size = 30
 info.text_size = 20
 stop.text_size = 35
 line2.text_size = 20
+line30.text_size = 30
 line3.text_size = 20
 image.resize(100,100)
+image2.resize(200,200)
 
 app.repeat(30*1000, testSpeed)
 app.bg = "yellow"
